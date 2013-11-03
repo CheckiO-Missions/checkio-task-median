0151474ffd5ab572835474a275e1fd0f827c63e5
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-11-03 16:30:37 +1200

    animation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -73,43 +73,93 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                 $content.find('.call').html('Pass: checkio(' + ext.JSON.encode(checkioInput) + ')');
                 $content.find('.answer').remove();
             }
-            //Dont change the code before it
 
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
-
+            var canvas = new MedianCanvas($content.find(".explanation")[0], checkioInput);
+            canvas.createCanvas();
+            canvas.animateCanvas();
 
             this_e.setAnimationHeight($content.height() + 60);
 
         });
 
-       
+        function normalizeArray(list) {
+            var res = [];
+            var m = Math.max.apply(Math, list);
+            for (var i = 0; i < list.length; i++) {
+                res.push(list[i] / m);
+            }
+            return res;
+        }
 
-        var colorOrange4 = "#F0801A";
-        var colorOrange3 = "#FA8F00";
-        var colorOrange2 = "#FAA600";
-        var colorOrange1 = "#FABA00";
+        function MedianCanvas(dom, numbers) {
+            var colorOrange4 = "#F0801A";
+            var colorOrange3 = "#FA8F00";
+            var colorOrange2 = "#FAA600";
+            var colorOrange1 = "#FABA00";
 
-        var colorBlue4 = "#294270";
-        var colorBlue3 = "#006CA9";
-        var colorBlue2 = "#65A1CF";
-        var colorBlue1 = "#8FC7ED";
+            var colorBlue4 = "#294270";
+            var colorBlue3 = "#006CA9";
+            var colorBlue2 = "#65A1CF";
+            var colorBlue1 = "#8FC7ED";
 
-        var colorGrey4 = "#737370";
-        var colorGrey3 = "#D9E9E";
-        var colorGrey2 = "#C5C6C6";
-        var colorGrey1 = "#EBEDED";
+            var colorGrey4 = "#737370";
+            var colorGrey3 = "#D9E9E";
+            var colorGrey2 = "#C5C6C6";
+            var colorGrey1 = "#EBEDED";
 
-        var colorWhite = "#FFFFFF";
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+            var colorWhite = "#FFFFFF";
+
+            var format = Raphael.format;
+
+            var cell = 30;
+            var maxHeight = 200;
+            var fontSize = 15;
+
+            var N = numbers.length;
+            var paper = Raphael(dom, (N - 1) * cell, maxHeight + fontSize * 2, 0, 0);
+            var numSet = [];
+
+            var attrLine = {"stroke": colorBlue3, "stroke-width": cell / 2, "stroke-linecap": "round"};
+            var attrText = {"stroke": colorBlue4, "font-size": fontSize, "font-family": "Verdana"};
+            var animationTime = 1000;
+            var delay = 300;
+
+            this.createCanvas = function () {
+                var normArray = normalizeArray(numbers);
+                for (var i = 0; i < N; i++) {
+                    var tempSet = paper.set();
+                    tempSet.push(paper.path(format(
+                        "M{0},{1}V{2}",
+                        i * cell,
+                        maxHeight,
+                        maxHeight * (1 - normArray[i])
+                    )).attr(attrLine));
+                    tempSet.n = numbers[i];
+                    tempSet.prevPos = i;
+                    tempSet.push(paper.text(i * cell, maxHeight + fontSize, numbers[i]).attr(attrText));
+                    numSet.push(tempSet);
+                }
+            };
+
+            this.animateCanvas = function () {
+                numSet.sort(function (a, b) {
+                    return a.n - b.n;
+                });
+                setTimeout(function () {
+                    for (var i = 0; i < N; i++) {
+                        var diff = i - numSet[i].prevPos;
+                        numSet[i].animate({"transform": "t" + (diff * cell) + ",0" }, animationTime);
+                    }
+                }, delay);
+                setTimeout(function() {
+                    numSet[Math.floor(N / 2)].animate({"stroke": colorOrange4, "fill": colorOrange4}, animationTime / 2);
+                    if (N % 2 == 0) {
+                        numSet[Math.floor(N / 2) - 1].animate({"stroke": colorOrange4, "fill": colorOrange4}, animationTime / 2);
+                    }
+                }, delay + animationTime);
+
+            }
+        }
 
 
     }
